a717d1ccf2c77cc5bb1aad75374f2386cec7c6e2
ineesgoncalvees <ines98goncalves@gmail.com>
2018-04-10 12:00:55 +0100

    Adicionar classe Peca

---
+++ Simplexity_Game/Peca.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplexity_Game {
+    public enum Cor {
+        Vermelho = 0,
+        Branco
+    }
+
+    public enum Forma {
+        Cilindro = 0,
+        Cubo
+    }
+
+    class Peca {
+        private Cor color;
+        private Forma shape;
+
+        public Peca(Cor color, Forma shape) {
+
+            this.color = color;
+            this.shape = shape;
+        }
+    }
+}

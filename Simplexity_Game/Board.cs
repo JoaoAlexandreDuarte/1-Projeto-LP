deebd2fcc0ad8aa42b9ccf34be96acdde1d427b2
FiammaV <ines.perdigao.nunes@gmail.com>
2018-04-18 18:04:32 +0100

    Adicionar a classe Board

---
+++ Simplexity_Game/Board.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Simplexity_Game {
+    /// <summary>
+    /// Class that creates the board and checks for the win condition
+    /// </summary>
+    public class Board {
+        // Array Piece that represents the board
+        public Piece[,] Board { get; }
+        
+        /// <summary>
+        /// Constructs the board
+        /// </summary>
+        public Board() {
+            //Initializes the board
+            Board = new Piece[7,7]; //hi friend :)
+        }
+
+        public void GetPiece(Piece piece, int column) {
+
+        }
+
+    }
+}

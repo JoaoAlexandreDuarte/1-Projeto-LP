5ca7f7ebb0770b4fc9da0162a02b149145f0acec
FiammaV <ines.perdigao.nunes@gmail.com>
2018-04-18 15:15:43 +0100

    Tested something to check the interface

--- Simplexity_Game/Program.cs
+++ Simplexity_Game/Program.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace Simplexity_Game {
     class Program {
         static void Main(string[] args) {
+            Interface board = new Interface();
+
+            board.ShowBoard();
+
+            Console.ReadKey();
         }
     }
 }

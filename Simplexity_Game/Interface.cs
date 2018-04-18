4808be37830a6ea7291e5d18fe538818bf98f9bd
João Duarte <onlyneplayer@gmail.com>
2018-04-19 00:02:47 +0100

    Melhorar a Interface mostrada ao utilizador

--- Simplexity_Game/Interface.cs
+++ Simplexity_Game/Interface.cs
@@ -2,38 +2,57 @@
 
 namespace Simplexity_Game {
     public class Interface {
+        private string square = "\u25a0  ";
+        private string circle = "O  ";
 
         public Interface() {
 
         }
 
-        public void ShowBoard() {
-            for(int i = 0; i < 7; i++) {
+        public void ShowCurrentPieces(Player player) {
+            Console.WriteLine("\nPlayer " + player.Number +
+                  " has a total of " + player.TotalPieces +
+                  " pieces to play right now (" + player.CubesNumber() +
+                  " cubes and " + player.CilindersNumber() + " cilinders");
+        }
+
+        public void ShowBoard(Piece[,] board) {
+            for (int i = 0; i < 7; i++) {
                 Console.Write(" ");
 
-                for(int j = 0; j < 7; j++) {
+                for (int j = 0; j < 7; j++) {
 
                     if (board[i, j] == null) {
-                        Console.Write(" | ")
-                    } else if(board[i,j].Shape == Shape.Cilinder) {
-                        if(board[i, j].Color == Color.White) {
-                            Console.Write("W O");
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Write("|  ");
+                        Console.ResetColor();
+                    } else if (board[i, j].Color == Color.White) {
+                        if (board[i, j].Shape == Shape.Cube) {
+                            Console.Write(square);
+                        } else if (board[i, j].Shape == Shape.Cilinder) {
+                            Console.Write(circle);
                         } else {
-                            Console.Write("R O");
+                            Console.Write("? ");
                         }
-                    } else if(board[i, j].Shape == Shape.Cube){
-                        if (board[i, j].Color == Color.White) {
-                            Console.Write("W D");
+                    } else if (board[i, j].Color == Color.Red) {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        if (board[i, j].Shape == Shape.Cube) {
+                            Console.Write(square);
+                        } else if (board[i, j].Shape == Shape.Cilinder) {
+                            Console.Write(circle);
+                        } else {
+                            Console.Write("?  ");
                         }
-                        else {
-                            Console.Write("R D");
-                        }
+                        Console.ResetColor();
                     } else {
-                        Console.Write(" u fucked up ");
+                        Console.Write("??");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(" ------------------- \n 1  2  3  4  5  6  7");
+            Console.ResetColor();
         }
     }
 }

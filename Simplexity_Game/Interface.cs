bdcb96a7ceaccf57b276c7ffbda790ca870a333b
FiammaV <ines.perdigao.nunes@gmail.com>
2018-04-18 15:12:14 +0100

    Made a test

--- Simplexity_Game/Interface.cs
+++ Simplexity_Game/Interface.cs
@@ -3,5 +3,37 @@
 namespace Simplexity_Game {
     class Interface {
 
+        public Interface() {
+
+        }
+
+        public void ShowBoard() {
+            for(int i = 0; i < 7; i++) {
+                Console.Write(" ");
+
+                for(int j = 0; j < 7; j++) {
+
+                    if (board[i, j] == null) {
+                        Console.Write(" | ")
+                    } else if(board[i,j].Shape == Shape.Cilinder) {
+                        if(board[i, j].Color == Color.White) {
+                            Console.Write("W O");
+                        } else {
+                            Console.Write("R O");
+                        }
+                    } else if(board[i, j].Shape == Shape.Cube){
+                        if (board[i, j].Color == Color.White) {
+                            Console.Write("W D");
+                        }
+                        else {
+                            Console.Write("R D");
+                        }
+                    } else {
+                        Console.Write(" u fucked up ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }

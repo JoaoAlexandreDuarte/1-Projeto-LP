60724cf56c0ee3604176cf79effa4a333a115e36
João Duarte <onlyneplayer@gmail.com>
2018-04-20 00:41:46 +0100

    Adicionar métodos extra de visualização e melhorar os antigos

--- Simplexity_Game/Interface.cs
+++ Simplexity_Game/Interface.cs
@@ -2,10 +2,12 @@
 
 namespace Simplexity_Game {
     /// <summary>
-    /// Class that renders the interface to the user
+    /// Class that renders the interface to the user such as the board and the
+    /// respective messages or errors
     /// </summary>
     public class Interface {
-        // Variables that keep the Cube and Cilinder shapes
+        // Variables that keep the Cube and Cilinder shapes so that's easy to
+        // change if needed
         private string square = "\u25a0  ";
         private string circle = "O  ";
 
@@ -17,43 +19,100 @@ namespace Simplexity_Game {
 
         }
 
-        public void ShowCurrentPieces(Player player) {
-            Console.WriteLine("\nPlayer " + player.Number +
+        /// <summary>
+        /// Renders the information available to the player
+        /// </summary>
+        public void ShowInfo(Player player) {
+            Console.WriteLine("Player " + (int)player.Number +
                   " has a total of " + player.TotalPieces +
                   " pieces to play right now (" + player.CubesNumber +
-                  " cubes and " + player.CilindersNumber + " cilinders");
+                  " cubes and " + player.CilindersNumber + " cilinders)");
         }
 
+        /// <summary>
+        /// Renders the question about the column in which the player wants to
+        /// play
+        /// </summary>
+        public void AskColumn() {
+            Console.WriteLine("\nAt which column do you want to place your" +
+                    " piece?");
+        }
+
+        /// <summary>
+        /// Renders the question about which piece the player wants to play
+        /// </summary>
+        public void AskPiece() {
+            Console.WriteLine("Which piece do you want to play? (1/Cube or " +
+                "2/Cilinder)");
+        }
+
+        /// <summary>
+        /// Renders the error for when the player inputs a wrong column number
+        /// </summary>
+        public void ErrorColumn() {
+            Console.WriteLine("\nNot a recognizeable column number! (Valid " +
+                "inputs are 1 up to 7)");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Renders the error for when the player inputs a wrong piece
+        /// </summary>
+        public void ErrorPiece() {
+            Console.WriteLine("\nNot a recognizeable piece! (Valid inputs" +
+                "1/Cube/cube OR 2/Cilinder/cilinder)");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Renders the board
+        /// </summary>
         public void ShowBoard(Piece[,] board) {
+            // Starts at the end so that the pieces "fall" down instead of 
+            // starting at the top, changing the point of view
             for (int i = 6; i >= 0; i--) {
                 Console.Write(" ");
 
                 for (int j = 0; j < 7; j++) {
 
+                    // Checks which piece is placed on the current board 
+                    // position and displays it
+
+                    // If there's no piece
                     if (board[i, j] == null) {
                         Console.ForegroundColor = ConsoleColor.DarkYellow;
                         Console.Write("|  ");
                         Console.ResetColor();
-                    } else if (board[i, j].Color == Color.White) {
-                        if (board[i, j].Shape == Shape.Cube) {
-                            Console.Write(square);
-                        } else if (board[i, j].Shape == Shape.Cilinder) {
-                            Console.Write(circle);
-                        } else {
-                            Console.Write("? ");
-                        }
-                    } else if (board[i, j].Color == Color.Red) {
+
+                        // If there's a White Cilinder
+                    } else if ((board[i, j].Color == Color.White) &&
+                        (board[i, j].Shape == Shape.Cilinder)) {
+                        Console.Write(circle);
+
+                        // If there's a  White Cube
+                    } else if ((board[i, j].Color == Color.White) &&
+                        (board[i, j].Shape == Shape.Cube)) {
+                        Console.Write(square);
+
+                        // If it's a Red Cube
+                    } else if ((board[i, j].Color == Color.Red) &&
+                        (board[i, j].Shape == Shape.Cube)) {
                         Console.ForegroundColor = ConsoleColor.Red;
-                        if (board[i, j].Shape == Shape.Cube) {
-                            Console.Write(square);
-                        } else if (board[i, j].Shape == Shape.Cilinder) {
-                            Console.Write(circle);
-                        } else {
-                            Console.Write("?  ");
-                        }
+                        Console.Write(square);
+                        Console.ResetColor();
+
+                        // If it's a Red Cilinder
+                    } else if ((board[i, j].Color == Color.Red) &&
+                        (board[i, j].Shape == Shape.Cilinder)) {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(circle);
                         Console.ResetColor();
                     } else {
-                        Console.Write("??");
+
+                        // If something goes wrong
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("? ");
+                        Console.ResetColor();
                     }
                 }
                 Console.WriteLine();

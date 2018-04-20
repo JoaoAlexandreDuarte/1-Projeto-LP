7e070353cdf2c614333ffd21ebab6c36a9bf9be7
João Duarte <onlyneplayer@gmail.com>
2018-04-20 01:00:36 +0100

    Adicionar a classe Checker vazia

---
+++ Simplexity_Game/Checker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Simplexity_Game {
+    /// <summary>
+    /// Class that will check if there's a win condition on the board
+    /// </summary>
+    class Checker {
+
+        public Checker() {
+
+        }
+
+        public Player CheckWin(Piece[,] board) {
+
+
+
+        }
+    }
+}

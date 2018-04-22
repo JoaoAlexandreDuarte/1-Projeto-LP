3418d1f81279e524d21587447589760f532859fd
FiammaV <ines.perdigao.nunes@gmail.com>
2018-04-22 16:12:13 +0100

    Corrected very minor mistakes in the comments

--- Simplexity_Game/Board.cs
+++ Simplexity_Game/Board.cs
@@ -36,18 +36,18 @@
         }
 
         /// <summary>
-        /// This method will check at which position the piece can be placed 
+        /// This method will check which position the piece can be placed in
         /// </summary>
         private int CheckColumn(int column) {
             // Starts at -1 so that it will return an error by default, any
-            // value that's not between 0 and 6 can be considered an error
+            // value that's not between 0 and 6 can be considered an error,
             // so we give it -1 because it's impossible to have negative values
             int row = -1;
 
             // Checks the given column for the spare space
             for (int i = 0; i < X; i++) {
 
-                // If the current space is null it will break the cicle and 
+                // If the current space is null, it will break the cicle and 
                 // return it
                 if (BoardArray[i, column] == null) {
                     row = i;
